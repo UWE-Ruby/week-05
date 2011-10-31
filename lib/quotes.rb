f17415d463e7307682a360d7f75316b7878a9cad
Franklin Webber <franklin.webber@gmail.com>
2011-10-30 18:57:27 -0700

    One possible solution for quotes

--- lib/quotes.rb
+++ lib/quotes.rb
@@ -16,6 +16,43 @@
 # @see http://rubydoc.info/stdlib/core/1.9.2/Module#alias_method-instance_method
 # 
 class Quotes
+  def initialize(options = {})
+    
+    @quotes = []
+    
+    options.each do |key,value|
+      send("#{key}=",value) if respond_to? "#{key}="
+    end
+  end
   
+  attr_accessor :file, :quotes
+  
+  alias_method :all, :quotes
+    
+  def find(linenumber)
+    if @quotes.empty? 
+      "Could not find a quote at this time"
+    else
+      @quotes[linenumber] || @quotes.sample
+    end
+  end
+  
+  alias_method :[], :find
+  
+  def file=(filename)
+    @filename = filename
+    
+    if File.exists? filename
+      @quotes = File.readlines(filename).map {|quote| quote.strip }
+    else
+      @quotes = []
+    end
+  end
+    
+  def search(params = {})
+    @quotes.map do |quote|
+      params.empty? ? quote : params.map {|key,value| quote if quote.send("#{key}?",value) }.uniq
+    end.flatten.compact
+  end
   
 end
